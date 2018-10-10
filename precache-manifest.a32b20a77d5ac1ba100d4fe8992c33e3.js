de4c8c6f08278c303feca6a8e2b8fcf0d8857074
Marina R <marina.rodkina1@yandex.ru>
2018-10-11 00:37:25 +0200

    Updates

---
+++ precache-manifest.a32b20a77d5ac1ba100d4fe8992c33e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "391f023bff2b32e6bf32",
+    "url": "/aviasales-app/static/js/runtime~main.391f023b.js"
+  },
+  {
+    "revision": "1ad1372eeec79b532ab7",
+    "url": "/aviasales-app/static/js/main.1ad1372e.chunk.js"
+  },
+  {
+    "revision": "6376e2b4f357873c35fe",
+    "url": "/aviasales-app/static/js/1.6376e2b4.chunk.js"
+  },
+  {
+    "revision": "1ad1372eeec79b532ab7",
+    "url": "/aviasales-app/static/css/main.94136c77.chunk.css"
+  },
+  {
+    "revision": "2a62d0e7401f5bbea8d203789952ad48",
+    "url": "/aviasales-app/index.html"
+  }
+];
